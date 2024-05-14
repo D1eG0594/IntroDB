55528fdaea83df9698d91865d50d58ce5a36de43
D1eG0594 <118742389+D1eG0594@users.noreply.github.com>
2024-05-14 17:19:25 -0500

    Correccion comentarios

--- ClaseSQL1.sql
+++ ClaseSQL1.sql
@@ -1,5 +1,5 @@
 
-#SQL
+--SQL
 Create database prueba1;
 USE prueba1;
 CREATE TABLE tabla1(
@@ -24,12 +24,12 @@ ALTER TABLE tabla1 DROP COLUMN password;
 
 DESCRIBE tabla1;
 
-#DML
+--DML
 
 INSERT INTO tabla1 (name, password) VALUES ('Diego', 1234)
 
-#------------------------------------------------------------------------------------------------------------------------------------------
-#Postgres
+--a------------------------------------------------------------------------------------------------------------------------------------------
+--Postgres
 Create database prueba1;
 USE prueba1;
 CREATE TABLE tabla1(
@@ -50,24 +50,24 @@ DROP SCHEMA prueba1; #Borrar esquema
 
 
 ALTER TABLE tabla1 ADD COLUMN password int; 
-ALTER TABLE tabla1 ALTER COLUMN password set DEFAULT 1234; #Modificar tabla
+ALTER TABLE tabla1 ALTER COLUMN password set DEFAULT 1234; --Modificar tabla
 ALTER TABLE tabla1 DROP COLUMN password;
 
 DESCRIBE tabla1;
 
-#DML
+--DML
 
 INSERT INTO tabla1 (name, password) VALUES ('Diego', 1234);
 INSERT INTO tabla1 (name, password) VALUES ('Jose', 7854);
 INSERT INTO tabla1 (name, password) VALUES ('Carlos', 5641);
 
-update tabla1 set password ='1234'; #Cambia todas las filas
+update tabla1 set password ='1234'; --Cambia todas las filas
 update tabla1 set password ='9876' where id='3';
 
-delete from tabla1 where name = 'Carlos';  #Eliminar
+delete from tabla1 where name = 'Carlos';  --Eliminar
 
 
-#DCL
+--DCL
 
 create user usuario1 with password 'contraseña';
 update user set password = 'nueva_contraseña'; where usename = 'usuario1';
@@ -76,15 +76,15 @@ drop user usuario1
 grant select, insert, update, delete on tabla1 to usuario1;
 revoke select. insert, update, delete on tabla1 from usuario1;
 
-#DQL
+--DQL
 
 select * from tabla1;
 select * from tabla1 where id='2';
-select * from tabla1 where name like '%j%'; #%a termina en a, a% empieza en a, %a% tiene a en cualquier parte, importa si es mayucula o minuscula
+select * from tabla1 where name like '%j%'; --%a termina en a, a% empieza en a, %a% tiene a en cualquier parte, importa si es mayucula o minuscula
 
-select name, password from tabla1; #Seleccionar los datos de una colomna o varias
+select name, password from tabla1; --Seleccionar los datos de una colomna o varias
 
-select name as tablaName from tabla1; # Modificar le nombre de una columna temporalmene
+select name as tablaName from tabla1; --Modificar le nombre de una columna temporalmene
 
 select count(*) from tabla1; #Saber cunatas filas tiene la tabla
 
